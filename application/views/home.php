d8eb7e135de8253e07c009fc9f8ad0e3436fea3c
anuj <anujrajput25@yahoo.com>
2013-10-27 12:45:14 +0530

    -layout is updadted -all the necessary theme files

---
+++ application/views/home.php
@@ -0,0 +1,470 @@
+<div id="slider">
+
+    <div class="container clearfix">
+
+
+        <div class="flexslider preloader2">
+
+
+            <div class="slider-wrap">
+
+
+                <div class="slide">
+
+                    <img src="images/slider/1.jpg" alt="Slider 1"/>
+
+                    <div class="slide-caption slide-caption2 slide-caption-left">
+
+                        <h3>Ultra Responsive Design</h3>
+
+                        <p>Nam ultricies dolor eu velit varius scelerisque. Vestibulum in lacus in felis pretium feugiat
+                            non sed elit.</p>
+
+                    </div>
+
+                </div>
+
+                <div class="slide">
+
+                    <img src="images/slider/2.jpg" alt="Slider 2"/>
+
+                    <div class="slide-caption">Bootstrap Components Compatible</div>
+
+                </div>
+
+                <div class="slide">
+
+                    <img src="images/slider/3.jpg" alt="Slider 3"/>
+
+                    <div class="slide-caption slide-caption2">
+
+                        <h3>InBuilt Power Features</h3>
+
+                        <p>Nam ultricies dolor eu velit varius scelerisque. Vestibulum in lacus in felis pretium feugiat
+                            non sed elit.</p>
+
+                    </div>
+
+                </div>
+
+                <div class="slide">
+
+                    <img src="images/slider/4.jpg" alt="Slider 4"/>
+
+                    <div class="slide-caption">Boxed &amp; Full Layouts</div>
+
+                </div>
+
+                <div class="slide">
+
+                    <img src="images/slider/5.jpg" alt="Slider 5"/>
+
+                    <div class="slide-caption caption-left">Unlimited Colors</div>
+
+                </div>
+
+
+            </div>
+
+
+        </div>
+
+
+        <div class="slider-line"></div>
+
+
+        <script type="text/javascript">
+
+            $(window).load(function () {
+
+                $('.flexslider').flexslider({
+
+                    selector: ".slider-wrap > .slide",
+                    animation: "slide",
+                    easing: "swing",
+                    direction: "horizontal",
+                    slideshow: true,
+                    slideshowSpeed: 7000,
+                    animationSpeed: 600,
+                    pauseOnHover: true,
+                    video: true,
+                    controlNav: false,
+                    directionNav: true,
+                    start: function (slider) {
+                        slider.removeClass('preloader2');
+                    }
+
+                });
+
+            });
+
+        </script>
+
+
+    </div>
+
+
+</div>
+
+
+<div id="content">
+
+
+<div class="content-wrap">
+
+
+<div class="container clearfix">
+
+
+<div class="promo">
+
+    <div class="promo-desc">
+
+        <h3>CoWorker is a Clean <span>Responsive</span> Multipurpose Theme</h3>
+
+        <span>Tons of Features, Premium Sliders, Retina Icons, Unlimited Colors &amp; Ample Layout Options.</span>
+
+    </div>
+
+    <div class="promo-action">
+        <a href="http://themes.semicolonweb.com/buy/coworker" target="_blank" class="simple-button inverse">Get it Now</a>
+    </div>
+
+</div>
+
+
+<div class="clear"></div>
+
+<!--services row starts here-->
+<div class="col_one_third">
+    <div class="product-feature product-feature3"><span class="icon-copy"></span>
+
+        <h3>85+ HTML Pages</h3>
+
+        <p>Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut
+            id elit.</p></div>
+</div>
+
+<div class="col_one_third">
+    <div class="product-feature product-feature3"><span class="icon-fullscreen"></span>
+
+        <h3>Fully Responsive</h3>
+
+        <p>Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut
+            id elit.</p></div>
+</div>
+
+<div class="col_one_third col_last">
+    <div class="product-feature product-feature3"><span class="icon-picture"></span>
+
+        <h3>12 Slider Plugins</h3>
+
+        <p>Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut
+            id elit.</p></div>
+</div>
+<!--services row end here-->
+
+<div class="clear"></div>
+
+
+<div class="clear"></div>
+
+
+<div class="tab_widget side-tabs clearfix bottommargin" id="tabwidget-1">
+
+<ul class="tabs">
+    <li><a href="#tab-tab1" data-href="#tab-tab1"><i class="icon-resize-full"></i> Responsive Design</a></li>
+    <li><a href="#tab-tab2" data-href="#tab-tab2"><i class="icon-font"></i> Google Fonts Support</a></li>
+    <li><a href="#tab-tab3" data-href="#tab-tab3"><i class="icon-picture"></i> 12 Awesome Sliders</a></li>
+    <li><a href="#tab-tab4" data-href="#tab-tab4"><i class="icon-pencil"></i> Unlimited Colors</a></li>
+    <li><a href="#tab-tab5" data-href="#tab-tab5"><i class="icon-beaker"></i> Bootstrap Components</a></li>
+    <li><a href="#tab-tab6" data-href="#tab-tab6"><i class="icon-th-large"></i> 2 Main Layouts</a></li>
+</ul>
+
+<div class="tab_container">
+
+    <div id="tab-tab1" class="tab_content clearfix">
+
+        <div class="wp-caption alignright notopmargin">
+
+            <img src="images/slider/sample.jpg" class="img-polaroid" alt="Image" title="Image"/>
+
+            <p class="wp-caption-text">Out of the Blue.</p>
+
+        </div>
+
+        <h3>Ultra Responsive Layouts</h3>
+
+        <p>Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula
+            ut id elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Duis mollis, est non
+            commodo luctus.Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh
+            ultricies vehicula ut id elit. Nulla vitae elit libero, a pharetra augue.</p>
+
+        <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus
+            posuere velit aliquet. Duis mollis, est non commodo luctus.Donec sed odio dui. Nulla vitae elit libero,
+            a pharetra augue.</p>
+
+    </div>
+
+
+    <div id="tab-tab2" class="tab_content clearfix">
+
+        <h3>Icons List</h3>
+
+        <div class="dropcap">D</div>
+        <p>onec sed odio dui. Nulla <span class="highlight ntip" title="ToolTip Example">vitae elit</span> libero, a
+            pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante
+            venenatis dapibus posuere velit aliquet. Duis mollis, est non commodo luctus. Donec sed odio dui. Nulla
+            vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Nulla vitae
+            elit libero, a pharetra augue.Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere
+            erat a ante venenatis dapibus posuere velit aliquet. Duis mollis, est non commodo luctus.</p>
+
+        <div class="nobottommargin entry_content">
+
+            <div class="col_one_fourth">
+
+                <ul class="nobottommargin">
+
+                    <li class="icon-reorder">Styled List 1</li>
+                    <li class="icon-reorder">Styled List 2</li>
+                    <li class="icon-reorder">Styled List 3</li>
+                    <li class="icon-reorder">Styled List 4</li>
+
+                </ul>
+
+            </div>
+
+            <div class="col_one_fourth">
+
+                <ul class="nobottommargin">
+
+                    <li class="icon-phone">Styled List 1</li>
+                    <li class="icon-phone">Styled List 2</li>
+                    <li class="icon-phone">Styled List 3</li>
+                    <li class="icon-phone">Styled List 4</li>
+
+                </ul>
+
+            </div>
+
+            <div class="col_one_fourth">
+
+                <ul class="nobottommargin">
+
+                    <li class="icon-search">Styled List 1</li>
+                    <li class="icon-search">Styled List 2</li>
+                    <li class="icon-search">Styled List 3</li>
+                    <li class="icon-search">Styled List 4</li>
+
+                </ul>
+
+            </div>
+
+            <div class="col_one_fourth col_last">
+
+                <ul class="nobottommargin">
+
+                    <li class="icon-download-alt">Styled List 1</li>
+                    <li class="icon-download-alt">Styled List 2</li>
+                    <li class="icon-download-alt">Styled List 3</li>
+                    <li class="icon-download-alt">Styled List 4</li>
+
+                </ul>
+
+            </div>
+
+
+        </div>
+
+    </div>
+
+    <div id="tab-tab3" class="tab_content clearfix">
+
+        <h3>12 Awesome Responsive Sliders</h3>
+
+        <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus
+            posuere velit aliquet. Duis mollis, est non commodo luctus. Aenean lacinia bibendum nulla sed
+            consectetur. Cras mattis consectetur purus sit amet fermentum.</p>
+
+        <div class="col_half entry_content ">
+
+            <ul class="bottommargin">
+
+                <li class="icon-ok">Layer Slider</li>
+                <li class="icon-ok">Revolution Slider</li>
+                <li class="icon-ok">Refine Slider</li>
+                <li class="icon-ok">Refine Thumb Slider</li>
+                <li class="icon-ok">Nivo Slider</li>
+                <li class="icon-ok">3D Slider</li>
+
+            </ul>
+
+        </div>
+
+        <div class="col_half col_last entry_content ">
+
+            <ul class="bottommargin">
+
+                <li class="icon-ok">Flex Slider</li>
+                <li class="icon-ok">Flex Thumb Slider</li>
+                <li class="icon-ok">Elastic Slider</li>
+                <li class="icon-ok">Camera Slider</li>
+                <li class="icon-ok">Accordion Slider</li>
+                <li class="icon-ok">Image/Video Static</li>
+
+            </ul>
+
+        </div>
+
+    </div>
+
+    <div id="tab-tab4" class="tab_content clearfix">
+
+        <h3>Unlimited Colors.</h3>
+
+        <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus
+            posuere velit aliquet. Duis mollis, est non commodo luctus. Aenean lacinia bibendum nulla sed
+            consectetur. Cras mattis consectetur purus sit amet fermentum.</p>
+
+        <div class="quote quote-left">Duis mollis, est non commodo luctus</div>
+        <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus
+            posuere velit aliquet. Duis mollis, est non commodo luctus. Aenean lacinia bibendum nulla sed
+            consectetur. Cras mattis consectetur purus sit amet fermentum. Nullam id dolor id nibh ultricies
+            vehicula ut id elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Duis mollis,
+            est non commodo luctus. Duis mollis, est non commodo luctus. Donec sed odio dui. Nulla vitae elit
+            libero, a pharetra augue.</p>
+
+    </div>
+
+    <div id="tab-tab5" class="tab_content clearfix">
+
+        <h3>Bootstrap Components.</h3>
+
+        <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus
+            posuere velit aliquet. Duis mollis, est non commodo luctus. Aenean lacinia bibendum nulla sed
+            consectetur. Cras mattis consectetur purus sit amet fermentum.</p>
+        <blockquote class="topmargin"><p>Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur
+                est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Duis
+                mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec
+                ullamcorper nulla non metus auctor fringilla.</p></blockquote>
+
+    </div>
+
+    <div id="tab-tab6" class="tab_content clearfix">
+
+        <h3>2 main Layouts.</h3>
+
+        <h6><span>Wide </span>Layout</h6>
+
+        <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Duis mollis, est non commodo luctus.
+            Aenean lacinia bibendum nulla sed consectetur. Cras mattis consectetur purus sit amet fermentum. Nullam
+            id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus posuere
+            velit aliquet. Duis mollis, est non commodo luctus.</p>
+
+        <h6><span>Boxed </span>Layout</h6>
+
+        <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus
+            posuere velit aliquet. Duis mollis, est non commodo luctus. Aenean lacinia bibendum nulla sed
+            consectetur. Cras mattis consectetur purus sit amet fermentum.</p>
+
+    </div>
+
+</div>
+
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+
+        tab_widget('#tabwidget-1');
+
+    });
+</script>
+
+<div class="clear"></div>
+
+
+<div style="position: relative;">
+
+    <h4>Our <span>Clients</span></h4>
+
+    <ul id="clients-scroller" class="our-clients clearfix">
+
+        <li><a href="http://www.logospire.com/logos/3122"><img src="images/clients/3122.jpg" alt="Client 1"
+                                                               title="Client 1"/></a></li>
+        <li><a href="http://www.logospire.com/logos/4044"><img src="images/clients/4044.png" alt="Client 2"
+                                                               title="Client 2"/></a></li>
+        <li><a href="http://www.logospire.com/logos/4122"><img src="images/clients/4112.jpg" alt="Client 3"
+                                                               title="Client 3"/></a></li>
+        <li><a href="http://www.logospire.com/logos/4147"><img src="images/clients/4147.jpg" alt="Client 4"
+                                                               title="Client 4"/></a></li>
+        <li><a href="http://www.logospire.com/logos/4487"><img src="images/clients/4487.png" alt="Client 5"
+                                                               title="Client 5"/></a></li>
+        <li><a href="http://www.logospire.com/logos/4999"><img src="images/clients/4999.jpg" alt="Client 6"
+                                                               title="Client 6"/></a></li>
+        <li><a href="http://www.logospire.com/logos/5055"><img src="images/clients/5055.jpg" alt="Client 7"
+                                                               title="Client 7"/></a></li>
+        <li><a href="http://www.logospire.com/logos/5393"><img src="images/clients/5393.jpg" alt="Client 8"
+                                                               title="Client 8"/></a></li>
+        <li><a href="http://www.logospire.com/logos/5909"><img src="images/clients/5909.png" alt="Client 7"
+                                                               title="Client 7"/></a></li>
+        <li><a href="http://www.logospire.com/logos/6002"><img src="images/clients/6002.png" alt="Client 8"
+                                                               title="Client 8"/></a></li>
+
+    </ul>
+
+    <div class="widget-scroll-prev" id="ourclients_prev"></div>
+    <div class="widget-scroll-next" id="ourclients_next"></div>
+
+    <script type="text/javascript">
+
+        jQuery(document).ready(function ($) {
+
+            var clientsCarousel = $("#clients-scroller");
+
+            clientsCarousel.carouFredSel({
+                width: "100%",
+                height: "auto",
+                circular: false,
+                responsive: true,
+                infinite: false,
+                auto: false,
+                items: {
+                    width: 160,
+                    visible: {
+                        min: 1,
+                        max: 6
+                    }
+                },
+                scroll: {
+                    wipe: true
+                },
+                prev: {
+                    button: "#ourclients_prev",
+                    key: "left"
+                },
+                next: {
+                    button: "#ourclients_next",
+                    key: "right"
+                },
+                onCreate: function () {
+                    $(window).on('resize',function () {
+                        clientsCarousel.parent().add(clientsCarousel).css('height', clientsCarousel.children().first().outerHeight() + 'px');
+                    }).trigger('resize');
+                }
+            });
+
+        });
+
+    </script>
+
+</div>
+
+
+</div>
+
+
+</div>
+
+
+</div>
