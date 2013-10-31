efacfa12c34f7a8733412d34f23117a413c74e34
anuj <anujrajput25@yahoo.com>
2013-10-31 23:13:45 +0530

    -completed about us

---
+++ application/views/about_us.php
@@ -0,0 +1,298 @@
+<div id="content">
+
+
+<div id="page-title">
+
+
+    <div class="container clearfix">
+
+        <h1>About Us<span>/ All you need to know about us</span></h1>
+
+
+    </div>
+
+
+</div>
+
+
+<div class="content-wrap">
+
+
+<div class="container clearfix">
+
+
+<div class="col_three_fifth">
+
+
+    <div class="fslider clearfix">
+
+        <div class="flexslider">
+
+
+            <div class="slider-wrap">
+
+
+                <div class="slide">
+
+                    <img src="<?php echo base_url('assets/images/services-1.jpg'); ?>" alt="Slider 2"/>
+
+                </div>
+
+                <div class="slide">
+
+                    <img src="<?php echo base_url('assets/images/services-2.jpg'); ?>" alt="Slider 3"/>
+
+                </div>
+
+                <div class="slide">
+
+                    <img src="<?php echo base_url('assets/images/services-3.jpg'); ?>" alt="Slider 4"/>
+
+                </div>
+
+
+            </div>
+
+
+        </div>
+
+    </div>
+
+
+</div>
+
+
+<div class="col_two_fifth col_last">
+
+
+    <div class="accordion clearfix nobottommargin">
+
+        <div class="acctitle">Our Mission</div>
+        <div class="acc_content clearfix">
+            <ul>
+                <li class="">To Become the Leading logistics Service provider with Global Presence.</li>
+                <li class="">To Become one of the complete logistics solution provider</li>
+                <li class="">To Become principal choice of agents</li>
+            </ul>
+        </div>
+
+        <div class="acctitle">Why Nolan?</div>
+        <div class="acc_content clearfix">Nolan specializes in Real Time Supply chain System. We
+            understand your exact needs as a Customer and what it takes to effectively support your Business in
+            terms of Efficient and on time Shipments. The processes we've developed are based on the
+            Architecture of “Always-On-Time” services which you can always rely on.
+        </div>
+
+        <div class="acctitle">What we do?</div>
+        <div class="acc_content clearfix">At Nolan, we are committed to delivering Operational Excellence to our
+            customers, each and every day. As a truly global and richly diverse organization, we work as one integrated
+            team. We focus on further growing our business by offering world-class supply chain services to our
+            customers and developing our business to support theirs. These are some of the ways we help make business
+            flow.
+        </div>
+
+        <div class="acctitle">How to get Support?</div>
+        <div class="acc_content clearfix">We are available 24/7, 365 days a
+            year and within 30 minutes of your enquiry, you will receive a first quotation. Our team of experienced and
+            dedicated professionals are always ready to take care of your consignment. Technical experts will make sure
+            that the right type of freight is used for your shipment, so you don't pay more than necessary.
+        </div>
+
+        <div class="acctitle">Where can you find us?</div>
+        <div class="acc_content clearfix">In order to contact us, you can simply call ua or drop us a mail.
+
+        </div>
+
+        <div class="acctitle">Why you choose our Company?</div>
+        <div class="acc_content clearfix">We at Nolan are committed to provide smooth services. We guarantee of
+            security & Punctuality on the worldwide routes. We turn transportation into a business advantage for our
+            ever progressing clients by delivering value added services that equip them to do effective logistic
+            planning & optimize their profit
+        </div>
+
+    </div>
+
+
+</div>
+
+
+<div class="dotted-divider"></div>
+
+
+<div class="col_two_fifth">
+
+
+    <h3>Our <span>Skills</span></h3>
+
+    <ul class="skills">
+
+        <li>
+
+            <span>Ocean Freight</span>
+
+            <div class="progress">
+                <div class="bar" data-width="80"></div>
+            </div>
+
+        </li>
+
+        <li>
+
+            <span>Air Freight</span>
+
+            <div class="progress">
+                <div class="bar bar-success" data-width="90"></div>
+            </div>
+
+        </li>
+
+        <li>
+
+            <span>Door-To-Door Delivery</span>
+
+            <div class="progress">
+                <div class="bar bar-warning" data-width="80"></div>
+            </div>
+
+        </li>
+
+        <li>
+
+            <span>Warehousing & Distribution</span>
+
+            <div class="progress">
+                <div class="bar bar-danger" data-width="70"></div>
+            </div>
+
+        </li>
+
+    </ul>
+
+
+</div>
+
+
+<div class="col_three_fifth col_last">
+
+
+    <div class="tab_widget nobottommargin" id="tabwidget-1">
+
+        <ul class="tabs">
+            <li><a href="#tab-tab1" data-href="#tab-tab1"><i class="icon-star"></i> Our Mission</a></li>
+            <li><a href="#tab-tab2" data-href="#tab-tab2"><i class="icon-question-sign"></i> Get Support</a>
+            </li>
+            <li><a href="#tab-tab3" data-href="#tab-tab3"><i class="icon-map-marker"></i> Locate Us</a></li>
+        </ul>
+
+        <div class="tab_container">
+
+            <div id="tab-tab1" class="tab_content clearfix">
+
+                <p>Integrity, honesty and politeness.Providing excellent service to all our customers.
+                    Empowering our team members to perform at their best.To make customer retention the driving force
+                    behind our organisation.</p>
+
+                <p class="nobottommargin">We believe that the key of success lies in nurturing vendor, employee,
+                    partner, and client relationships and establishing a positive work environment conveying happiness
+                    for our team and clients all throughout.</p></div>
+
+            <div id="tab-tab2" class="tab_content clearfix">
+
+                <div class="col_two_fifth nobottommargin"><img
+                        src="<?php echo base_url('assets/images/support.jpg'); ?>"
+                        class="notopmargin norightmargin img-polaroid" alt="Image"
+                        title="Image"></div>
+                <div class="col_three_fifth col_last">
+
+                    You can contact us anytime and our customer care executive will listen all your queries.
+
+                    <ul class="nobottommargin">
+
+                        <li class="icon-thumbs-up">Live Support (10 AM to 6 PM)</li>
+                        <li class="icon-thumbs-up">Phone Support (10 AM to 10 PM)</li>
+                        <li class="icon-thumbs-up">Email Support (24x7)</li>
+                        <li class="icon-thumbs-up">1800-4624-24</li>
+
+                    </ul>
+
+                </div>
+
+            </div>
+
+            <div id="tab-tab3" class="tab_content clearfix">
+                <div class="col_two_fifth nobottommargin">
+
+                    <iframe class="img-polaroid" width="100%" height="155" frameborder="0" scrolling="no"
+                            marginheight="0" marginwidth="0"
+                            src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=envato&amp;aq=&amp;sll=22.612365,88.378443&amp;sspn=0.013371,0.026157&amp;t=m&amp;ie=UTF8&amp;hq=envato&amp;ll=-37.817941,144.964977&amp;spn=0.017629,0.032015&amp;output=embed"></iframe>
+
+                </div>
+
+                <div class="col_three_fifth col_last">
+
+                    You can easily locate us at:
+
+                    <ul class="nobottommargin">
+
+                        <li class="icon-map-marker">46-AB Tingira Street <br>Smith Tower,Port Smith,QLD-4870<br>
+                            Australia
+                        </li>
+
+                        <li class="icon-envelope-alt">nolan@gmail.com</li>
+
+                    </ul>
+
+                </div>
+
+                <div class="clear"></div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <script type="text/javascript">
+
+        $(document).ready(function () {
+
+            tab_widget('#tabwidget-1');
+
+        });
+
+    </script>
+
+    <div class="clear"></div>
+
+
+</div>
+
+
+<div class="clear"></div>
+
+
+<div class="promo clearfix" style="margin-top: 20px;">
+
+    <div class="promo-desc">
+
+        <h3>Call us today at <span>+91.22.57412541</span> or Email us at <span>support@nolan.com</span></h3>
+
+        <span>We strive to provide our customers with Top Notch Support to make their Experience Wonderful</span>
+
+    </div>
+
+    <div class="promo-action"><a href="#">Contact Us</a></div>
+
+</div>
+
+
+<div class="clear"></div>
+
+
+</div>
+
+
+</div>
+
+
+</div>
